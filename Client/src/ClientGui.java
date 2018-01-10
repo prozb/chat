846fb9e78f9977ba2e7181d47c378dc6a919cf38
= <rozbich15@gmail.com>
2018-01-10 11:17:00 +0100

    trying to make gui better

--- Client/src/ClientGui.java
+++ Client/src/ClientGui.java
@@ -10,22 +10,78 @@ import javafx.scene.layout.VBox;
 import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class ClientGui extends Application implements ISendable {
     private Stage primaryStage;
-    private Scene scene;
-    private VBox layout;
+    private Scene secondScene;
+    private Scene firstScene;
+    private VBox secondLayout;
+    private VBox firstLayout;
     private TextArea textArea;
+    private TextArea firtsTextArea;
     private Button connectButton;
     private Button stopButton;
     private Button sendButton;
     private TextField sendField;
+    private TextField connectField;
     private boolean clientStarted;
     private Client client;
+    private boolean isLoggedIn;
+    private Pattern pattern;
+    private Matcher matcher;
 
-    @Override
-    public void init() throws Exception {
-        this.clientStarted = false;
+
+    private void initFirstScene(){
         this.connectButton = new Button("Connect");
+        this.connectButton.setPrefWidth(130);
+
+        this.connectButton.setOnAction(e -> {
+            if(!clientStarted) {
+                this.clientStarted = true;
+                try {
+                    this.client = new Client(ClientGui.this);
+                } catch (Exception e1) {
+                    sendTextToGui("cannot establish connection");
+                    this.clientStarted = false;
+                }
+                String msg = connectField.getText();
+                if (!isLoggedIn && !msg.equals("")) {
+                    client.sendString(Constants.CONNECT + ": " + msg);
+                }
+            }
+        });
+
+        this.sendField = new TextField();
+        this.sendField.setEditable(true);
+        this.sendField.setPrefColumnCount(100);
+
+        this.connectField = new TextField("Input your name");
+
+        this.firstLayout = new VBox();
+        this.firstLayout.setAlignment(Pos.CENTER);
+        this.firstLayout.setPadding(new Insets(10,25,25,25));
+        this.firstScene = new Scene(firstLayout, 500, 300, Color.BLACK);
+
+        this.firtsTextArea = new TextArea();
+        this.firtsTextArea.setEditable(false);
+        this.firtsTextArea.setWrapText(true);
+        this.firtsTextArea.setScrollTop(10);
+        this.firtsTextArea.setPrefHeight(230);
+
+        HBox hBox1 = new HBox();
+        hBox1.setPadding(new Insets(10,0,25,0));
+        hBox1.getChildren().add(firtsTextArea);
+        HBox hBox2 = new HBox();
+        hBox2.getChildren().add(connectField);
+        hBox2.setPadding(new Insets(10,0,25,0));
+        HBox hBox3 = new HBox();
+        hBox3.getChildren().addAll(connectButton);
+        hBox3.setSpacing(70);
+        this.firstLayout.getChildren().addAll(hBox1, hBox2, hBox3);
+    }
+    private void initSecondScene(){
         this.sendButton = new Button("Send");
         this.sendButton.setOnAction(e -> {
             String msg = sendField.getText();
@@ -35,26 +91,15 @@ public class ClientGui extends Application implements ISendable {
             }
         });
         this.sendButton.setPrefWidth(130);
-        this.connectButton.setPrefWidth(130);
 
-        this.connectButton.setOnAction(e -> {
-            if(!clientStarted) {
-                try {
-                    this.client = new Client(ClientGui.this);
-                    this.clientStarted = true;
-                } catch (Exception e1) {
-                    sendTextToGui("cannot establish connection!");
-                    this.clientStarted = false;
-                }
-            }else {
-                sendTextToGui("you are already connected!");
-            }
-        });
         this.stopButton = new Button("Stop");
         this.stopButton.setOnAction(e -> {
             if(clientStarted){
                 client.sendString(Constants.DISCONNECT + ":");
                 this.clientStarted = false;
+                this.isLoggedIn = false;
+                this.firtsTextArea.appendText(textArea.getText());
+                this.primaryStage.setScene(firstScene);
             }else{
                 sendTextToGui("you must establish connection!");
             }
@@ -71,10 +116,10 @@ public class ClientGui extends Application implements ISendable {
         this.sendField.setEditable(true);
         this.sendField.setPrefColumnCount(100);
 
-        this.layout = new VBox();
-        this.layout.setAlignment(Pos.CENTER);
-        this.layout.setPadding(new Insets(10,25,25,25));
-        scene = new Scene(layout, 500, 300, Color.BLACK);
+        this.secondLayout = new VBox();
+        this.secondLayout.setAlignment(Pos.CENTER);
+        this.secondLayout.setPadding(new Insets(10,25,25,25));
+        secondScene = new Scene(secondLayout, 500, 300, Color.BLACK);
 
         HBox hBox1 = new HBox();
         hBox1.setPadding(new Insets(10,0,25,0));
@@ -83,9 +128,18 @@ public class ClientGui extends Application implements ISendable {
         hBox2.getChildren().add(sendField);
         hBox2.setPadding(new Insets(10,0,25,0));
         HBox hBox3 = new HBox();
-        hBox3.getChildren().addAll(connectButton, sendButton, stopButton);
+        hBox3.getChildren().addAll(sendButton, stopButton);
         hBox3.setSpacing(70);
-        this.layout.getChildren().addAll(hBox1, hBox2, hBox3);
+        this.secondLayout.getChildren().addAll(hBox1, hBox2, hBox3);
+    }
+
+    @Override
+    public void init() throws Exception {
+        this.isLoggedIn = false;
+
+        initFirstScene();
+        initSecondScene();
+        //this.firstScene.setOnC
 
         super.init();
     }
@@ -96,12 +150,37 @@ public class ClientGui extends Application implements ISendable {
         primaryStage.setResizable(false);
         primaryStage.setTitle("Client");
 
-        this.primaryStage.setScene(scene);
+
+        this.primaryStage.setScene(getScene());
         this.primaryStage.show();
+    }
+    private Scene getScene(){
+        if(isLoggedIn){
+            return secondScene;
+        }else{
+            return firstScene;
+        }
     }
 
     @Override
     public void sendTextToGui(String text){
-        textArea.appendText(text + "\n");
+        if(isLoggedIn) {
+            textArea.appendText(text + "\n");
+        }else {
+            if(isConnect(text) && !isLoggedIn) {
+                isLoggedIn = true;
+                this.textArea.appendText(firtsTextArea.getText());
+                //primaryStage.setScene(getScene());
+            }else{
+                this.isLoggedIn = false;
+            }
+            firtsTextArea.appendText(text + "\n");
+        }
+    }
+
+    public boolean isConnect(String msg){
+        pattern = Pattern.compile("^connect: ok$");
+        matcher = pattern.matcher(msg);
+            return matcher.matches();
     }
 }
